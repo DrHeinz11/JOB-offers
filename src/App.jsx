03c53d74868390e8bb95e4736cc76d3025183a50
DrHeinz11 <franco.martin.1998m@gmail.com>
2022-07-11 14:42:04 -0300

    V1.1

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import { Heading } from "@chakra-ui/react";
+function App() {
+  return <Heading color="primary.bground">Hola esto es una prueba </Heading>;
+}
+
+export default App;
---
+++ src/theme.jsx
@@ -0,0 +1,12 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  primary: {
+    dkGray: "hsl(180,14%,20%)",
+    lgGray: "hsl(180,52%,96%)",
+    filterTable: "hsl(180,31%,95%)",
+    bground: "hsl(180,52%,96%)",
+  },
+};
+const theme = extendTheme({ colors });
+export default theme;
