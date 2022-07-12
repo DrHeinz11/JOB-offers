65abfbdb89176e7f02c431998be2b6e634ddfb20
DrHeinz11 <franco.martin.1998m@gmail.com>
2022-07-12 02:22:57 -0300

    V1.7(Fix)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Heading, Image, Button, Stack } from "@chakra-ui/react";
+import { Heading, Image, Button, Stack, Container } from "@chakra-ui/react";
 import Card from "./components/Card";
 import { data, dataArr, dataTec } from "./constant/dataExport";
 import { useState } from "react";
@@ -10,43 +10,50 @@ function App() {
   });
 
   return (
-    <Stack bg="primary.lgGray">
-      <Stack direction="column">
-        <Stack direction="row">
-          {dataArr.map((e, index) => (
-            <Button
-              size="lg"
-              textTransform="capitalize"
-              colorScheme="teal"
-              onClick={() => setArgument(e)}
-              key={index}
-            >
-              {e}
-            </Button>
-          ))}
-        </Stack>
+    <Container bg="primary.lgGray" maxW="100%">
+      <Stack maxWidth="1200px" m={"0 auto"}>
+        <Stack direction="column">
+          <Stack direction="row" justify={{ base: "center", md: "flex-start" }}>
+            {dataArr.map((e, index) => (
+              <Button
+                size="lg"
+                textTransform="capitalize"
+                colorScheme="teal"
+                onClick={() => setArgument(e)}
+                key={index}
+              >
+                {e}
+              </Button>
+            ))}
+          </Stack>
 
-        <Stack direction="row">
-          {dataTec.map((e, index) => (
-            <Button
-              textTransform="capitalize"
-              size="md"
-              colorScheme="teal"
-              onClick={() => setArgument(e.toLowerCase())}
-              key={index}
-            >
-              {e}
-            </Button>
-          ))}
+          <Stack
+            direction="row"
+            flexWrap="wrap"
+            gap="10px"
+            justify={{ base: "center", md: "flex-start" }}
+          >
+            {dataTec.map((e, index) => (
+              <Button
+                textTransform="capitalize"
+                size="md"
+                colorScheme="teal"
+                onClick={() => setArgument(e.toLowerCase())}
+                key={index}
+              >
+                {e}
+              </Button>
+            ))}
+          </Stack>
         </Stack>
+        <div className="contianer">
+          {}
+          {arrRetorno.map((e, index) => (
+            <Card props={e} key={index} />
+          ))}
+        </div>
       </Stack>
-      <div className="contianer">
-        {}
-        {arrRetorno.map((e, index) => (
-          <Card props={e} key={index} />
-        ))}
-      </div>
-    </Stack>
+    </Container>
   );
 }
 
--- src/images/svg-icons/export.js
+++ src/images/svg-icons/export.js
@@ -8,3 +8,5 @@ export { default as eyeCam } from "./eyecam-co.svg";
 export { default as airFilter } from "./the-air-filter-company.svg";
 export { default as photoSnapshot } from "./photosnap.svg";
 export { default as account } from "./account.svg";
+export { default as bgHeaderDeks } from "./bg-header-desktop.svg";
+export { default as bgHeaderMob } from "./bg-header-mobile.svg";
